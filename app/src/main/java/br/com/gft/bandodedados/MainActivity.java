125efbe36c91a03166465ab121874b973961dc3e
leandro-gft <lpsacchi@gmail.com>
2020-06-02 13:54:36 -0300

    Utilizando comandos do SQLite

--- app/src/main/java/br/com/gft/bandodedados/MainActivity.java
+++ app/src/main/java/br/com/gft/bandodedados/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.gft.bandodedados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            //Criar banco de dados
+            SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+
+            //Criar tabela
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3))");
+
+            //Inserir dados
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Leandro Sacchi', 31)");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Jamilton Damasceno', 30)");
+
+            //Recuperar dados
+            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas ", null);
+
+            //Indices da tabela
+            int indiceNome = cursor.getColumnIndex("nome");
+            int indiceIdade = cursor.getColumnIndex("idade");
+
+            cursor.moveToFirst();
+            while (cursor != null) {
+                Log.i("RESULTADO - nome: ", cursor.getString(indiceNome));
+                Log.i("RESULTADO - idade: ", cursor.getString(indiceIdade));
+                cursor.moveToNext();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
